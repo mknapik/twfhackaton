58b5a2873cfbaf0f56e9d4f285d8913c2e26d418
Michał Knapik <michal.knapik@u2i.com>
2015-09-05 18:21:29 +0200

    add commas

--- app/assets/javascripts/services/OwlCarousel.js
+++ app/assets/javascripts/services/OwlCarousel.js
@@ -8,11 +8,11 @@ app.directive('owlCarousel', ['$timeout', '$rootScope', function ($timeout, $roo
                     autoPlay: parseInt(attrs.autoplay) || 10000,
                     responsive: true,
                     pagination: true,
-                    navigation: false
+                    navigation: false,
                     navigationText: [
                       "<i class='fa fa-angle-left'></i>",
                       "<i class='fa fa-angle-right'></i>"
-                    ],
+                    ]
                 });
             });
         }
