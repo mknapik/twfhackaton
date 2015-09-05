956ef311ee885f8e008b0ed81f4ac83118f54832
mswiatkowa <zamojska.mz@gmail.com>
2015-09-05 17:39:36 +0000

    endgame popup

--- app/assets/javascripts/controllers/PagesCtrl.js
+++ app/assets/javascripts/controllers/PagesCtrl.js
@@ -8,10 +8,8 @@ Page1Ctrl = function($scope,$timeout, getGame, postGame, $routeParams) {
 
   getGame.get(function(data){
       $scope.description = data.description;
-      console.log(data);
       $scope.categories = data.tile_sets;
       $scope.tiles = data.tile_sets[$scope.currentCategory].tiles;
-
       $timeout(function(){
         DraggModule.makeDraggable();
       });
@@ -28,18 +26,23 @@ Page1Ctrl = function($scope,$timeout, getGame, postGame, $routeParams) {
       TweenMax.to($(".task-popup"), 0.6, {  css:{zIndex:-1}, ease:Linear.easeNone});
       TweenMax.to($(".open-popup"), 0.6, {  scale: 1,x:01, y:0,z:0, ease:Linear.easeNone});
   }
+  $scope.closePopupRating = function($event) {
+      TweenMax.to($(".rating-popup"), 0.6, {  opacity: 0, ease:Linear.easeNone});
+      TweenMax.to($(".rating-popup"), 0.6, {  css:{zIndex:-1}, ease:Linear.easeNone});
+  }
   $scope.playSound = function($event) {
       $(".task-text").speak();
   }
   $scope.finishGame = function($event) {
       postGame.post(function(data){
-        console.log(data);
+        $scope.rating = data.data.rating;
+        console.log(data.data.rating);
           TweenMax.to($(".rating-popup"), 1, {  opacity: 1, ease:Linear.easeNone});
           TweenMax.to($(".rating-popup"), 1, {  css:{zIndex:1000}, ease:Linear.easeNone,}); 
-      });
+      },$routeParams.id);
+  }
+  $scope.playAgain = function($event) {
   }
 };
-
-
 app.controller('Page1Ctrl', ['$scope', '$timeout', 'getGame', 'postGame', '$routeParams', Page1Ctrl]);
 
