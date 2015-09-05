f9bd7bb73af47368859d69f1367a0805216ddd33
Michał Knapik <michal.knapik@u2i.com>
2015-09-05 18:40:51 +0200

    Restore switchCategory

--- app/assets/javascripts/controllers/PagesCtrl.js
+++ app/assets/javascripts/controllers/PagesCtrl.js
@@ -22,15 +22,20 @@ Page1Ctrl = function ($scope, $timeout, getGame, postGame, $routeParams) {
         TweenMax.to($(".task-popup"), 1, {opacity: 1, ease: Linear.easeNone, delay: 0.5});
         TweenMax.to($(".task-popup"), 1, {css: {zIndex: 1000}, ease: Linear.easeNone, delay: 0.5});
         TweenMax.to($(event.target), 1, {opacity: 1, ease: Linear.easeNone, delay: 2});
-    }
+    };
     $scope.closePopup = function ($event) {
         TweenMax.to($(".task-popup"), 0.6, {opacity: 0, ease: Linear.easeNone});
         TweenMax.to($(".task-popup"), 0.6, {css: {zIndex: -1}, ease: Linear.easeNone});
         TweenMax.to($(".open-popup"), 0.6, {scale: 1, x: 01, y: 0, z: 0, ease: Linear.easeNone});
-    }
+    };
     $scope.playSound = function ($event) {
         $(".task-text").speak();
-    }
+    };
+    $scope.switchCategory = function($event) {
+        var id = $(event.target).attr("id");
+        $scope.currentCategory = id;
+        $scope.tiles = $scope.data.tile_sets[$scope.currentCategory].tiles;
+    };
     $scope.finishGame = function ($event) {
         postGame.post(function (data) {
             console.log(data);
