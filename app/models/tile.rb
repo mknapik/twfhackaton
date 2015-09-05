2d0d4c14b8daa988a2346fac7715dec86bf5312d
Michał Knapik <michal.knapik@u2i.com>
2015-09-05 11:23:24 +0200

    Add image to Tile and TileSet

--- app/models/tile.rb
+++ app/models/tile.rb
@@ -2,6 +2,7 @@ class Tile < ActiveRecord::Base
   has_and_belongs_to_many :tile_sets
   has_many :solutions
 
+  has_attached_file :image
   validates :name, presence: true
-  validates :image, attachment_presence: true
+  validates_attachment :image, presence: true
 end
--- app/models/tile_set.rb
+++ app/models/tile_set.rb
@@ -2,6 +2,7 @@ class TileSet < ActiveRecord::Base
   belongs_to :game
   has_and_belongs_to_many :tiles
 
+  has_attached_file :image
   validates :name, :game_id, presence: true
-  validates :image, attachment_presence: true
+  validates_attachment :image, presence: true
 end
