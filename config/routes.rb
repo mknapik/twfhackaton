ba21e880d2417206ccf2fbc56c9c848aad18b672
Marcin Swiatek <marcin.swiatek@u2i.com>
2015-09-05 16:28:08 +0200

    Move tiles to admin

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   root 'main#index'
 
-  resources :tiles, only: [:index, :new, :create]
-
   scope :api do
     resources :games, only: [:index, :show] do
       get :preview
     end
+  end
+
+  scope :admin do
+    resources :tiles, only: [:index, :new, :create]
   end
 end
