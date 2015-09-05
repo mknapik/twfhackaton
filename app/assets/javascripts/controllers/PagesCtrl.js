060343e96f13f87386d8c901fcfda3aa4e062bc5
mswiatkowa <zamojska.mz@gmail.com>
2015-09-05 12:08:05 +0000

    Add popup

--- app/assets/javascripts/controllers/PagesCtrl.js
+++ app/assets/javascripts/controllers/PagesCtrl.js
@@ -4,15 +4,20 @@ app = angular.module('twf');
 
 Page1Ctrl = function($scope) {
   $scope.title = "Hello, page1!";
-  $scope.popupIsVisible = false;
   $scope.openPopup = function($event) {
-      console.log(event.target);
-      TweenMax.to($(event.target), 3, {  opacity: 0, ease:Linear.easeNone})
-      TweenMax.to($(event.target).parent(), 3, {  scale: 40, ease:Linear.easeNone})
-      TweenMax.to($(event.target).parent(), 5, {  x:1000, y:1000,z:-20, ease:Linear.easeNone})
-      //$scope.popupIsVisible = true;
+      TweenMax.to($(event.target), 0.2, {  opacity: 0, ease:Linear.easeNone});
+      TweenMax.to($(event.target).parent(), 0.6, {  scale: 40,x:900, y:900,z:-20, ease:Linear.easeNone});
+      TweenMax.to($(".task-popup"), 1, {  opacity: 1, ease:Linear.easeNone, delay: 0.5});
+      TweenMax.to($(".task-popup"), 1, {  css:{zIndex:1000}, ease:Linear.easeNone, delay: 0.5}); 
+      TweenMax.to($(event.target), 1, {  opacity: 1, ease:Linear.easeNone,delay: 2});
+  }
+  $scope.closePopup = function($event) {
+      TweenMax.to($(".task-popup"), 0.6, {  opacity: 0, ease:Linear.easeNone});
+      TweenMax.to($(".task-popup"), 0.6, {  css:{zIndex:-1}, ease:Linear.easeNone}); 
+      TweenMax.to($(".open-popup"), 0.6, {  scale: 1,x:01, y:0,z:0, ease:Linear.easeNone});
   }
 };
+
 
 Page2Ctrl = function($scope) {
   return $scope.title = "Yo, page2!";
