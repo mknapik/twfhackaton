184dd129d5149b2c4ca99f52a61dd2e2e0eddad2
mswiatkowa <zamojska.mz@gmail.com>
2015-09-05 15:19:09 +0000

    Game view

--- app/assets/javascripts/controllers/PagesCtrl.js
+++ app/assets/javascripts/controllers/PagesCtrl.js
@@ -7,10 +7,9 @@ Page1Ctrl = function($scope,getGame) {
   $scope.currentCategory = 0;
   getGame.get(function(data){
       $scope.description = data.description;
-      console.log(data);
       $scope.categories = data.tile_sets;
+      $scope.data = data;
       $scope.tiles = data.tile_sets[$scope.currentCategory].tiles;
-     
   });
   $scope.openPopup = function($event) {
       TweenMax.to($(event.target), 0.2, {  opacity: 0, ease:Linear.easeNone});
@@ -20,12 +19,18 @@ Page1Ctrl = function($scope,getGame) {
       TweenMax.to($(event.target), 1, {  opacity: 1, ease:Linear.easeNone,delay: 2});
   }
   $scope.closePopup = function($event) {
-      TweenMax.to($(".task-popup"), 0.6, {  opacity: 0, ease:Linear.easeNone});
-      TweenMax.to($(".task-popup"), 0.6, {  css:{zIndex:-1}, ease:Linear.easeNone}); 
-      TweenMax.to($(".open-popup"), 0.6, {  scale: 1,x:01, y:0,z:0, ease:Linear.easeNone});
+      TweenMax.to($(".task-popup"), 0.4, {  opacity: 0, ease:Linear.easeNone});
+      TweenMax.to($(".task-popup"), 0.4, {  css:{zIndex:-1}, ease:Linear.easeNone}); 
+      TweenMax.to($(".open-popup"), 0.4, {  scale: 1,x:01, y:0,z:0, ease:Linear.easeNone});
   }
   $scope.playSound = function($event) {
-      $(".task-text").speak();
+      event.stopPropagation();
+      $(event.target).parent().parent().speak();
+  }
+  $scope.switchCategory = function($event) {
+      var id = $(event.target).attr("id");
+      $scope.currentCategory = id;
+      $scope.tiles = $scope.data.tile_sets[$scope.currentCategory].tiles;
   }
 };
 
