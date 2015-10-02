240be40f07687bcb764d8b39fab837cc73afde4b
Marcin Swiatek <marcin.swiatek@u2i.com>
2015-10-02 23:12:49 +0200

    Dodawanie gier

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,78 +23,64 @@ def initialize_game_types
 end
 
 
-def create_tile(name, image)
+def dodaj_obrazek(name, image)
   Tile.where(name: name).first_or_create! do |tile|
     tile.image = open("images/#{image}")
   end
 end
 
-def create_subset_game(name, description)
+def stworz_gre_typu_podzbior(name, description)
   Game.where(name: name, game_type_id: 1).first_or_create! do |game|
     game.description = description
   end
 end
 
-def create_category(name, game, tiles)
+def dodaj_kategorie_dla_gry(game, name, tiles)
   TileSet.where(name: name, game: game).first_or_create! do |set|
     set.tiles << tiles
     set.image = open('http://lorempixel.com/200/200/food/')
   end
 end
 
-def create_solution(tile, game, set)
-  Solution.where(tile_id: tile.id, game_id: game.id).first_or_create! do |s|
-    s.set = set
+def ustaw_odpowiedz_dla_gry(game, tile)
+  Solution.where(tile_id: tile.id, game_id: game.id).first_or_create!
+end
+
+class Gra
+
+  def self.stworz(name, description)
+    Gra.new(name, description)
+  end
+
+  def initialize(name , description)
+    @gra = stworz_gre_typu_podzbior(name, description)
+  end
+
+  def ma_poprawna_odpowiedz(tile)
+    ustaw_odpowiedz_dla_gry(@gra, tile)
+  end
+
+  def dodaj_kategorie(name, tiles)
+    dodaj_kategorie_dla_gry(@gra, name, tiles)
   end
 end
 
 initialize_game_types
 
 # TWorzenie gry śniadanie
-sniadanie = create_subset_game('Śniadanie', 'Wybierz produkty na śniadanie')
+sniadanie = Gra.stworz('Śniadanie', 'Wybierz produkty na śniadanie')
 
 ## Dodawanie obrazkow
-chleb = create_tile('chleb', 'chleb.png')
-bulka = create_tile('bulka', 'bulka.png')
-jogurt = create_tile('jogurt', 'jogurt.png')
-ser = create_tile('ser', 'ser.png')
+chleb = dodaj_obrazek('chleb', 'chleb.png')
+bulka = dodaj_obrazek('bulka', 'bulka.png')
+jogurt = dodaj_obrazek('jogurt', 'jogurt.png')
+ser = dodaj_obrazek('ser', 'ser.png')
 
 ## Tworzenie kategorii
-nabial = create_category('Nabiał', sniadanie, [bulka, jogurt])
-pieczywo = create_category('Pieczywo', sniadanie, [chleb, ser])
+sniadanie.dodaj_kategorie('Nabiał', [bulka, jogurt])
+sniadanie.dodaj_kategorie('Pieczywo' , [chleb, ser])
 
 ## Tworzenie poprawnych odpowiedzi
+sniadanie.ma_poprawna_odpowiedz(chleb)
+sniadanie.ma_poprawna_odpowiedz(ser)
 
-create_solution(chleb, sniadanie, 1)
-create_solution(ser, sniadanie, 2)
-create_solution(bulka, sniadanie, 2)
-create_solution(jogurt, sniadanie, 1)
-
-#
-# if Dir.exists?(Rails.root.join('tmp/education'))
-#
-#   tiles = Dir.glob('tmp/education/*').map do |file_name|
-#     puts file_name
-#     name = file_name.sub('tmp/education', '').gsub('.svg', '')
-#     Tile.where(name: name).first_or_create! do |tile|
-#       tile.image = open(file_name)
-#     end
-#   end
-#
-#   (1..10).each do |i|
-#     education = Game.where(name: "Edukacja #{i}", game_type_id: 2).first_or_create! do |game|
-#       game.description = ''
-#     end
-#
-#     sample = tiles.sample(50)
-#
-#     TileSet.where(name: 'Edukacja', game: Game.find_by!(name: "Edukacja #{i}")).first_or_create! do |set|
-#       set.tiles << sample
-#       set.image = open('tmp/education/education.png')
-#     end
-#
-#     sample.take(20).each do |tile|
-#       Solution.where(tile_id: tile.id, game_id: education.id).first_or_create!
-#     end
-#   end
-# end
